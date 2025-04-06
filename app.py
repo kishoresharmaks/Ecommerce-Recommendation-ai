4dcaca29ca24e3afa9f8e3a6991127e2e47a0cad
Kishoresharma <140733947+kishoresharmaks@users.noreply.github.com>
2025-04-06 16:29:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
-import main  
+import main
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    main.generate_recommendations()  
-    return render_template('recommendations.html')  
+    
+    main.main()  
+    
+    with open('recommendations.html', 'r', encoding='utf-8') as f:
+        html_content = f.read()
+    return html_content
 
 if __name__ == '__main__':
     app.run(debug=True)
